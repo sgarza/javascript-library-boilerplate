0a4e31a6038a4eae2a2dcf0452e999655356a903
Sergio de la Garza <sergio@delagarza.io>
2019-08-30 18:47:49 -0500

    Lint

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,13 @@
-import { expect } from 'chai';
-import { sum } from '../src/index';
+import { expect } from "chai";
+import { sum } from "../src/index";
 
-describe('sum', () => {
-  it('should return the sum of 2 numbers', () => {
+describe("sum", () => {
+  it("should return the sum of 2 numbers", () => {
     const a = 5;
     const b = 11;
 
     const result = sum(a, b);
 
     expect(result).to.be.equal(a + b);
-  })
-})
+  });
+});
